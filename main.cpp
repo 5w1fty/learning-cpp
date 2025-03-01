d8aed26fe19cd18ab6a14c074497e12071bfa9ad
JP_Development <julius.pfleging@gmail.com>
2025-03-01 15:02:10 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+float addition(float a, float b) {
+
+	float c = NULL;
+	c = a + b;
+	return c;
+}
+
+float subtraction(float a, float b) {
+	float c = NULL;
+	c = a - b;
+	return c;
+}
+
+float multiplication(float a, float b) {
+	float c = NULL;
+	c = a * b;
+	return c;
+}
+
+float division(float a, float b) {
+	float c = NULL;
+	c = a / b;
+	return c;
+}
+
+int main(int argc, char* argv[]) {
+
+}
